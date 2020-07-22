b489464ec814abc3a7d87160dffd5e84db759cb3
ayush884 <ayush@whatsbusy.com>
2020-07-22 13:46:34 -0700

    float added

--- primitives/float.go
+++ primitives/float.go
@@ -1,8 +1,13 @@
 // float32 float64
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	temp := 1
+	fmt.Printf("float32(%v) => Type: %T | Value: %v | Size: %d bits\n", temp, float32(temp), float32(temp), unsafe.Sizeof(float32(temp)))
+	fmt.Printf("float64(%v) => Type: %T | Value: %v | Size: %d bits\n", temp, float64(temp), float64(temp), unsafe.Sizeof(float64(temp)))
 }
--- primitives/int.go
+++ primitives/int.go
@@ -21,10 +21,30 @@ func main() {
 
 	// format a string without printing it
 	// fmt.Printf takes type string in argument
-	fmt.Printf("%v 	=> Type: %T | Value: %v | Size: %d bits\n", temp, temp, temp, unsafe.Sizeof(temp))
+	fmt.Printf("%v => Type: %T | Value: %v | Size: %d bits\n\n", temp, temp, temp, unsafe.Sizeof(temp))
+	// 1 => Type: int | Value: 1 | Size: 8 bits
+
 	fmt.Printf("uint(%v) => Type: %T | Value: %v | Size: %d bits\n", temp, uint(temp), uint(temp), unsafe.Sizeof(uint(temp)))
 	fmt.Printf("uint8(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, uint8(temp), uint8(temp), unsafe.Sizeof(uint8(temp)))
 	fmt.Printf("uint16(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, uint16(temp), uint16(temp), unsafe.Sizeof(uint16(temp)))
 	fmt.Printf("uint32(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, uint32(temp), uint32(temp), unsafe.Sizeof(uint32(temp)))
-	fmt.Printf("uint64(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, uint64(temp), uint64(temp), unsafe.Sizeof(uint64(temp)))
+	fmt.Printf("uint64(%v) => Type: %T | Value: %v | Size: %d bytes\n\n", temp, uint64(temp), uint64(temp), unsafe.Sizeof(uint64(temp)))
+
+	// uint(1) => Type: uint | Value: 1 | Size: 8 bits
+	// uint8(1) => Type: uint8 | Value: 1 | Size: 1 bytes
+	// uint16(1) => Type: uint16 | Value: 1 | Size: 2 bytes
+	// uint32(1) => Type: uint32 | Value: 1 | Size: 4 bytes
+	// uint64(1) => Type: uint64 | Value: 1 | Size: 8 bytes
+
+	fmt.Printf("int(%v) => Type: %T | Value: %v | Size: %d bits\n", temp, int(temp), int(temp), unsafe.Sizeof(int(temp)))
+	fmt.Printf("int8(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, int8(temp), int8(temp), unsafe.Sizeof(int8(temp)))
+	fmt.Printf("int16(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, int16(temp), int16(temp), unsafe.Sizeof(int16(temp)))
+	fmt.Printf("int32(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, int32(temp), int32(temp), unsafe.Sizeof(int32(temp)))
+	fmt.Printf("int64(%v) => Type: %T | Value: %v | Size: %d bytes\n", temp, int64(temp), int64(temp), unsafe.Sizeof(int64(temp)))
+
+	// int(1) => Type: int | Value: 1 | Size: 8 bits
+	// int8(1) => Type: int8 | Value: 1 | Size: 1 bytes
+	// int16(1) => Type: int16 | Value: 1 | Size: 2 bytes
+	// int32(1) => Type: int32 | Value: 1 | Size: 4 bytes
+	// int64(1) => Type: int64 | Value: 1 | Size: 8 bytes
 }
