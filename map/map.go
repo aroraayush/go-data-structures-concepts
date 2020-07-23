2ecac9f8cc9642df9fd8711029311f202cd2d682
Ayush Arora <ayusharora009@gmail.com>
2020-07-23 12:42:25 -0700

    Update map.go

--- map/map.go
+++ map/map.go
@@ -1,3 +1,8 @@
+// Golang Maps is a collection of unordered pairs of key-value
+// - Keys are reference to a hash table
+// - Due to its reference type it is inexpensive to pass, for example, for a 64-bit machine it takes 8 bytes and for a 32-bit machine, it takes 4 bytes.
+
+
 package main
 
 import (
